af0a79b280f6ca50c387cdb0b7cc1cd6c1228aff
Nícolas <nicolasbrga01@gmail.com>
2023-02-28 14:06:20 -0300

    (Fix) Req 1 - Consertando o fetch

--- src/services/fetchPlanets.js
+++ src/services/fetchPlanets.js
@@ -1,8 +1,8 @@
 const fetchPlanets = async () => {
   const planets = await fetch('https://swapi.dev/api/planets');
-  const { data } = await planets.json();
-  await data.forEach((planet) => delete planet.residents);
-  return data;
+  const { results } = await planets.json();
+  await results.forEach((planet) => delete planet.residents);
+  return results;
 };
 
 export default fetchPlanets;
